5cd703d91c404b3e9ba53fcbbd7ff5ee47b39f1c
Karl-Martin Skontorp <kms@skontorp.net>
2011-08-13 14:06:00 +0200

    Update homepage reference.

--- connection-bw-monitor.rb
+++ connection-bw-monitor.rb
@@ -2,7 +2,7 @@
 # connection-bw-monitor
 # Monitor download bandwidth.
 #
-# (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://picofarad.org/
+# (c) Karl-Martin Skontorp <kms@skontorp.net> ~ http://22pf.org/
 # Licensed under the GNU GPL 2.0 or later.
 
 require 'http-access2'
